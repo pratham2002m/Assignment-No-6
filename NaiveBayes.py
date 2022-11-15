23f5efda74d28302795f4e991bb3344f38d3d7f5
pratham2002m <85343542+pratham2002m@users.noreply.github.com>
2022-11-15 13:19:29 +0530

    Naive bayes classifier

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,116 +1,112 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 24 00:10:16 2019
-
-@author: Sadman Sakib
-"""
-import globalVars_q2 as gv 
 import pandas as pd
-#Split dataframe into positive and negative
-dfs_SplitByClasses=[]
-for region, df_region in gv.dfTrain.groupby('BUY'):
-    dfs_SplitByClasses.append(df_region)
-X_Train_Negative=dfs_SplitByClasses[0]
-X_Train_Positive=dfs_SplitByClasses[1]
-X_Train_Negative=X_Train_Negative.iloc[:, 0:len(gv.dfTrain.columns)-1] 
-X_Train_Positive=X_Train_Positive.iloc[:, 0:len(gv.dfTrain.columns)-1] 
-trainLengthNegative=X_Train_Negative.shape[0]
-trainLengthPositive=X_Train_Positive.shape[0]
-#***Find unique values in each feature***
-def findUniqueValueDict():
-    for colIndex in range(len(gv.allColumns)):
-        uniqueValFound_df_fullds=(gv.X[gv.allColumns[colIndex]].value_counts())     
-        eachColAllPossValList=list()
-        for items in uniqueValFound_df_fullds.iteritems(): 
-            eachColAllPossValList.append(items[0])
-        gv.valuesFullDS[gv.allColumns[colIndex]]=eachColAllPossValList   
-def findExtra(value):
-        m=len(value)
-        p=1/m    
-        return m, p
-#***Peroformance Measure***
-def performanceCalculation(y_actual, y_predicted):
-    TP = FP = TN = FN =  0
-    for i in range(len(y_actual)): 
-        if y_actual[i]==y_predicted[i]==gv.labels[0]:
-           TP += 1
-        elif y_actual[i]==y_predicted[i]==gv.labels[1]:
-           TN += 1
-        elif y_predicted[i]==gv.labels[0] and y_actual[i]!=y_predicted[i]:
-           FP += 1
-        elif y_predicted[i]==gv.labels[1] and y_actual[i]!=y_predicted[i]:
-           FN += 1
-    T_N=TP + FP + TN + FN
-    print("Accuracy =", (((TP+TN)/T_N)*100),"%")
-    print("Sensitivity =", ((TP)/(TP + FN)))
-    print("Specificity =", ((TN)/(TN + FP)))
-#***PRIORS Calculation***
-def priorCalculate():
-    global prior_P, prior_N
-    classValueCount=gv.dfTrain['BUY'].value_counts()
-    prior_P=classValueCount[gv.labels[0]]/(classValueCount[gv.labels[0]]+classValueCount[gv.labels[1]])
-    prior_N=classValueCount[gv.labels[1]]/(classValueCount[gv.labels[0]]+classValueCount[gv.labels[1]])
+import numpy as np
+from sklearn.metrics import accuracy_score
 
-#***Likelyhood calculation***
-def likelyhoodCalculate(typeVal):
-    for key, value in gv.valuesFullDS.items():
-        likelyhood=list()
-        frequencyList=list()
-        for eachValue in value:
-            if(typeVal=='N'):
-                count=(X_Train_Negative[key] == eachValue).sum()
-                trainLen=trainLengthNegative
-            else:
-                count=(X_Train_Positive[key] == eachValue).sum()
-                trainLen=trainLengthPositive
-            frequencyList.append(count)
-            m,p=findExtra(value)
-            likelyhood.append((count+(m*p))/(trainLen+m))     
-        data = {'Value':value, 'Frequency':frequencyList, 'Likelyhood':likelyhood}    
-        df_Objects = pd.DataFrame(data) 
-        if(typeVal=='N'):
-            gv.likelyhood_N[key]=df_Objects 
-        else:
-            gv.likelyhood_P[key]=df_Objects   
-#**** Find posterior ****            
-def calculatePosterior(posterior, likelyhood, items):
-        df_Found=likelyhood[items[0]]
-        foundRow=df_Found.loc[df_Found['Value'] == items[1]]
-        individualLikelyhood=foundRow['Likelyhood']
-        individualLikelyhood=individualLikelyhood.values[0]
-        posterior=posterior*individualLikelyhood 
-        return posterior
-#***    Find probability to belong in each classes***
-def calculateTestProbability(testRow):
-    global individualLikelyhood
-    global posterior_N
-    global posterior_P
-    individualLikelyhood=0
-    posterior_P=prior_P
-    posterior_N=prior_N
-    for items in testRow.iteritems():
-        posterior_N=calculatePosterior(posterior_N, gv.likelyhood_N, items)
-        posterior_P=calculatePosterior(posterior_P, gv.likelyhood_P, items)
-#*** TESTING ***
-predictions=list()
-def testPrediction():
-    for index, row in gv.X_test.iterrows():
-        calculateTestProbability(row)
-        posterior_N_normalize=(posterior_N)/(posterior_N+posterior_P)
-        posterior_P_normalize=(posterior_P)/(posterior_N+posterior_P)
-        if(posterior_N_normalize>posterior_P_normalize):
-            predictions.append(gv.labels[1])
-        else:
-            predictions.append(gv.labels[0])                
-findUniqueValueDict()     
-priorCalculate()   
-#*** NEGATIVE LIKELYHOOD CALCULATION ***
-likelyhoodCalculate('N')
-#*** POSITIVE LIKELYHOOD CALCULATION ***
-likelyhoodCalculate('P')
-testPrediction()
-#*** PERFORMANCE Measure ***
-performanceCalculation(list(gv.Y_test), predictions)
-gv.dfTest['predictions']=predictions
-#*** Save predictions file ***
-gv.dfTest.to_csv("Predictions.csv", sep=',',index = None)
+dfTrain = pd.read_csv("q2_train.csv")
+dfTest = pd.read_csv("q2_test.csv")
+
+X = dfTrain.iloc[:, 0:len(dfTrain.columns)-1] 	  
+Y = dfTrain['BUY'] 	  
+
+
+
+
+
+unique_table = []
+final_table = []
+final_prob_table = []
+    
+
+def train_data() : 
+    for i in range(len(dfTrain.columns)) : 
+        arr = np.array(dfTrain.iloc[:, i:i+1] )
+        # print(arr)
+        cnt_unique = np.unique(arr)
+        unique_table.append(cnt_unique)
+
+    print(unique_table)
+        
+
+    for i in range(len(dfTrain.columns)) : 
+        arr = np.array(dfTrain.iloc[:, i:i+1],dtype=int)
+        ouput = np.array(dfTrain.iloc[:, -1],dtype=int)
+        temp1 = [] 
+        temp2 = [] 
+        # print(i)
+        for j in range(len(unique_table[i])) : 
+            # print(j)
+            temp1.append(0)
+            temp2.append(0)
+
+        for j in range(len(arr)) : 
+            
+            if ouput[j] == 0 :
+                temp1[arr[j][0]]  = temp1[arr[j][0]]  + 1 
+            else : 
+                temp2[arr[j][0]]  = temp2[arr[j][0]]  + 1
+
+        temp = [temp1,temp2]
+        final_table.append(temp)
+
+    
+
+    for i in range(len(final_table)) : 
+        temp1 = []
+        temp2 = []
+        for j in range(len(unique_table[i])) : 
+            print(final_table[i][0][j]/final_table[-1][0][0])
+            val = final_table[i][0][j]/final_table[-1][0][0]
+            temp1.append(val)
+        for j in range(len(unique_table[i])) : 
+            print(final_table[i][1][j]/final_table[-1][0][0])
+            val = final_table[i][1][j]/final_table[-1][1][1]
+            temp2.append(val)
+        
+        temp = [] 
+        temp.append(temp1)
+        temp.append(temp2)
+
+        final_prob_table.append(temp)
+    print(final_table)
+    print(final_prob_table)
+
+def predict(features) : 
+    # print("final = ",final_table)
+    yes = final_table[-1][1][1]/len(X)
+    no = final_table[-1][0][0]/len(X)
+    # print(yes)
+
+    for i in range(len(final_table)-1) : 
+        no = no*final_prob_table[i][0][features[i]]
+        yes = yes*final_prob_table[i][1][features[i]]
+        # print(yes)
+    
+    # print(yes)
+    # print(no)
+    if yes > no : 
+        return 1
+    else : 
+        return 0
+
+def test_data() : 
+    X = dfTrain.iloc[:, 0:len(dfTrain.columns)-1] 	  
+    Y = dfTrain.iloc[:, -1]
+    Y_predict = []
+    for i in range(len(X)) : 
+        features = dfTrain.iloc[i, 0:len(dfTrain.columns)-1]
+        # print(type(features.values))
+        predicted_val = predict(features.values)
+        Y_predict.append(predicted_val)
+        print(predicted_val,Y[i])
+    
+
+    
+    ans = accuracy_score(Y, Y_predict)
+    print(ans)
+
+train_data()
+test_data()
+
+
+
+
